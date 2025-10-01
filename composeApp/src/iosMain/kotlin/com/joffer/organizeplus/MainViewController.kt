5bcb2cde6bcb7556ee38a53ce05638d4c36c813e
felipe <felipejmartins@gmail.com>
2025-09-30 23:29:41 -0300

    Fix iOS crash: initialize Koin dependency injection
    - Add Koin initialization in MainViewController for iOS
    - Use lazy delegate to ensure single initialization
    - Prevents 'KoinApplication has not been started' crash

--- composeApp/src/iosMain/kotlin/com/joffer/organizeplus/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/joffer/organizeplus/MainViewController.kt
@@ -1,5 +1,19 @@
 package com.joffer.organizeplus
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.joffer.organizeplus.di.appModule
+import com.joffer.organizeplus.di.platformDatabaseModule
+import org.koin.core.context.startKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+// Initialize Koin once using lazy - guaranteed single execution
+private val koinApplication = lazy {
+    startKoin {
+        modules(appModule, platformDatabaseModule)
+    }
+}
+
+fun MainViewController() = ComposeUIViewController {
+    // Initialize Koin before first composition
+    koinApplication.value
+    App()
+}

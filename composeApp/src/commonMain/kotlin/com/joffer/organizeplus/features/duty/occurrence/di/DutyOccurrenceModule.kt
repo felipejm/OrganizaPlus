64511c3cafea5e33858f19f279cdabb429d98766
felipe <felipejmartins@gmail.com>
2025-10-08 16:02:03 -0300

    fix: register DutyOccurrenceRepository in Koin
    Fixed app crash on startup caused by missing Koin definition.
    
    - Added DutyOccurrenceRepository registration in dutyOccurrenceModule
    - Repository now properly injected into DashboardViewModel and AddDutyOccurrenceViewModel
    - Using RoomDutyOccurrenceRepository implementation
    
    Fixes: InstanceCreationException when creating DashboardViewModel

--- composeApp/src/commonMain/kotlin/com/joffer/organizeplus/features/duty/occurrence/di/DutyOccurrenceModule.kt
+++ composeApp/src/commonMain/kotlin/com/joffer/organizeplus/features/duty/occurrence/di/DutyOccurrenceModule.kt
@@ -1,9 +1,19 @@
 package com.joffer.organizeplus.features.duty.occurrence.di
 
+import com.joffer.organizeplus.features.duty.occurrence.data.repositories.RoomDutyOccurrenceRepository
+import com.joffer.organizeplus.features.duty.occurrence.domain.repositories.DutyOccurrenceRepository
 import com.joffer.organizeplus.features.duty.occurrence.presentation.AddDutyOccurrenceViewModel
 import org.koin.dsl.module
 
 val dutyOccurrenceModule = module {
+    // Repository
+    single<DutyOccurrenceRepository> {
+        RoomDutyOccurrenceRepository(
+            dutyOccurrenceDao = get()
+        )
+    }
+
+    // ViewModels
     factory { (dutyId: Long) ->
         AddDutyOccurrenceViewModel(
             dutyOccurrenceRepository = get(),
